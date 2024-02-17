ff0fd6993b4a155f87d21e1a5b7443058c97b281
mahmudsadek <mahmoudsadek.1905@gmail.com>
2024-02-17 20:37:41 +0200

    add changes to the main

--- BQMS/BQMS.cpp
+++ BQMS/BQMS.cpp
@@ -1,11 +1,7 @@
 // BQMS.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
-#include "customer.h"
-#include "Queue.h"
-#include "Taller.h"
-#include <vector>
-#include<cmath>
+#include "Application.h"
 using namespace std;
 
 int Taller::count = 0;
@@ -15,36 +11,9 @@ int Taller::count = 0;
 
 int main()
 {
-	
-	Queue* Customers = new Queue();
-	vector <Customer*> custumersArr{
-		new Customer("Mahmoud",23,PLATINUM,8,23),
-		new Customer("Sadek",63,GOLD,9,12),
-		new Customer("Ali",63,PLATINUM,9,23),
-		new Customer("Ahmed",53,REGULER,10,2),
-		new Customer("ibrahem",62,PLATINUM,11,33),
-		new Customer("Taha",23,PLATINUM,12,14),
-		new Customer("Taha",69,PLATINUM,12,14),
-
-	};
-	
-	
-	/*char ch;
-	do
-	{
-		custumersArr.push_back(AddCustomer());
-		cout << "Continue ? Y - N : ";
-		cin >> ch;
-	} 
-	while (ch != 'n' && ch != 'N');*/
-
-	for (int i = 0; i < custumersArr.size(); i++)
-	{
-		Customers->enqueue(custumersArr[i]);
-		Customers->Display();
-	}
+	Application *app = new Application();
+	app->Run();
 }
-
 
 
 
