e128c253feb4ec9b06807fd1d77cd5292743cfce
mahmudsadek <mahmoudsadek.1905@gmail.com>
2024-02-17 20:37:08 +0200

    Edit the Bank class

--- BQMS/Bank.cpp
+++ BQMS/Bank.cpp
@@ -1,1 +1,25 @@
 #include "Bank.h"
+#include <Queue>
+
+void Bank::AddCustomer(std::string name, int age, AccountType accType, int arrTimeHour, int arrTimeMin)
+{
+	Customers.push(new Customer(name,age,accType,arrTimeHour,arrTimeMin));
+}
+
+Bank::Bank()
+{
+}
+
+void Bank::AddTaller()
+{
+	tallers.push_back(new Taller());
+}
+
+std::priority_queue<Customer*, std::vector<Customer*>, Compare> Bank::getCustomers()
+{
+	std::priority_queue < Customer*, std::vector<Customer*>, Compare> tempQueue = Customers;
+	return tempQueue;
+}
+
+
+
--- BQMS/Bank.h
+++ BQMS/Bank.h
@@ -11,6 +11,9 @@ private:
 	std::list<Taller*> tallers;
 	std::priority_queue<Customer*, std::vector<Customer*>, Compare> Customers;
 public:
-	
+	Bank();
+	void AddCustomer(std::string name, int age, AccountType accType, int arrTimeHour, int arrTimeMin);
+	void AddTaller();
+	std::priority_queue<Customer*, std::vector<Customer*>, Compare> getCustomers();
 };
 
